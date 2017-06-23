3c4db645e988e22132f7e7c2e7f2530292e786e8
Eric <eric.giguere@calculquebec.ca>
2017-06-23 11:54:16 -0400

    fast-sto

--- qutip/cy/fast_stochastic.pyx
+++ qutip/cy/fast_stochastic.pyx
@@ -9,24 +9,25 @@ include "parameters.pxi"
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def _smesolve_single_trajectory(n, sso):
+def _smesolve_single_trajectory(int n, object sso):
     """
     Internal function. See smesolve.
     """
-    dt = sso.dt
-    times = sso.times
-    d1, d2 = sso.d1, sso.d2
-    d2_len = sso.d2_len
-    L_data = sso.L.data
-    N_substeps = sso.N_substeps
-    N_store = sso.N_store
-    A_ops = sso.A_ops
+    cdef double dt = sso.dt
+    cdef np.ndarray[double, ndim=2] times = sso.times
+    cdef int d2_len = sso.d2_len
+    cdef int N_substeps = sso.N_substeps
+    cdef int N_store = sso.N_store
+    cdef complex s
+    cdef object A_ops = sso.A_ops
 
-    rho_t = mat2vec(sso.state0.full()).ravel()
+    cdef np.ndarray[complex, ndim=1] rho_t = mat2vec(sso.state0.full()).ravel()
     dims = sso.state0.dims
 
-    expect = np.zeros((len(sso.e_ops), sso.N_store), dtype=complex)
-    ss = np.zeros((len(sso.e_ops), sso.N_store), dtype=complex)
+    cdef np.ndarray[complex, ndim=2] expect = np.zeros((len(sso.e_ops),
+                                            sso.N_store), dtype=complex)
+    cdef np.ndarray[complex, ndim=2] ss = np.zeros((len(sso.e_ops),
+                                            sso.N_store), dtype=complex)
 
     # reseed the random number generator so that forked
     # processes do not get the same sequence of random numbers
@@ -37,24 +38,24 @@ def _smesolve_single_trajectory(n, sso):
             dW = sso.generate_noise(len(A_ops), N_store, N_substeps,
                                     sso.d2_len, dt)
         elif sso.homogeneous:
-            #if sso.distribution == 'normal':
             dW = np.sqrt(dt) * np.random.randn(len(A_ops), N_store,
                                                N_substeps, d2_len)
-            #else:
-            #    raise TypeError('Unsupported increment distribution for ' +
-            #                    'homogeneous process.')
         else:
-            #if sso.distribution != 'poisson':
-            #    raise TypeError('Unsupported increment distribution for ' +
-            #                    'inhomogeneous process.')
-
             dW = np.zeros((len(A_ops), N_store, N_substeps, d2_len))
     else:
         dW = sso.noise[n]
 
     states_list = []
-    measurements = np.zeros((len(times), len(sso.s_m_ops), d2_len),
-                            dtype=complex)
+    cdef np.ndarray[complex, ndim=3] measurements = np.zeros((len(times),
+                            len(sso.s_m_ops), d2_len), dtype=complex)
+
+    if(sso.rhs in (10, 20, 21, 22)):
+        A_data = A[0][0].data
+        A_ind = A[0][0].indices
+        A_ptr = A[0][0].indptr
+        A_size = A[0][1]
+        A_len = len(A)
+
 
     for t_idx, t in enumerate(times):
 
@@ -67,21 +68,31 @@ def _smesolve_single_trajectory(n, sso):
         if sso.store_states or not sso.s_e_ops:
             states_list.append(Qobj(vec2mat(rho_t), dims=dims))
 
-        rho_prev = np.copy(rho_t)
+        cdef np.ndarray[complex, ndim=1] rho_prev = np.copy(rho_t)
 
-        for j in range(N_substeps):
+        if sso.rhs == 10:
+            for j in range(N_substeps):
+                rho_t = _rhs_rho_euler_homodyne_fast(rho_t, A_data,
+                                A_ind, A_ptr, A_size,
+                                dW[:, t_idx, j, 0])
+        elif sso.rhs == 20:
+            for j in range(N_substeps):
+                rho_t = _rhs_rho_milstein_homodyne_single_fast(rho_t, A_data,
+                                A_ind, A_ptr, A_size,
+                                dW[:, t_idx, j, 0])
+        elif sso.rhs == 21:
+            for j in range(N_substeps):
+                rho_t = _rhs_rho_milstein_homodyne_two_fast(rho_t, A_data,
+                                A_ind, A_ptr, A_size,
+                                dW[:, t_idx, j, 0])
+        elif sso.rhs == 22:
+            for j in range(N_substeps):
+                rho_t = _rhs_rho_milstein_homodyne_fast(rho_t, A_data,
+                                A_ind, A_ptr, A_size, A_len,
+                                dW[:, t_idx, j, 0])
 
-            if sso.noise is None and not sso.homogeneous:
-                for a_idx, A in enumerate(A_ops):
-                    dw_expect = cy_expect_rho_vec(A[4], rho_t, 1) * dt
-                    if dw_expect > 0:
-                        dW[a_idx, t_idx, j, :] = np.random.poisson(dw_expect,
-                                                                   d2_len)
-                    else:
-                        dW[a_idx, t_idx, j, :] = np.zeros(d2_len)
 
-            rho_t = sso.rhs(L_data, rho_t, t + dt * j,
-                            A_ops, dt, dW[:, t_idx, j, :], d1, d2, sso.args)
+
 
         if sso.store_measurement:
             for m_idx, m in enumerate(sso.s_m_ops):
@@ -104,17 +115,16 @@ def _smesolve_single_trajectory(n, sso):
 @cython.wraparound(False)
 cdef _rhs_rho_euler_homodyne_fast(complex[::1] rho_t,
                                 complex[::1] A_data,
-                                int[::1] A_ind, int[::1] A_ptr, 
-                                int A_size, int A_len,
-                                np.ndarray[double, ndim=2] ddW):
+                                int[::1] A_ind, int[::1] A_ptr,
+                                int A_size,
+                                np.ndarray[double, ndim=1] dW):
     """
     Fast Euler-Maruyama for homodyne detection.
     """
-
-    np.ndarray[double, ndim=1] dW = ddW[:, 0]
-
-    d_vec = spmv_csr(A_data, A_ind, A_ptr, rho_t).reshape(-1, len(rho_t))
-    e = d_vec[:-1].reshape(-1, A_size, A_size).trace(axis1=1, axis2=2)
+    cdef np.ndarray[complex, ndim=2] d_vec =
+        spmv_csr(A_data, A_ind, A_ptr, rho_t).reshape(-1, len(rho_t))
+    cdef np.ndarray[double, ndim=1] e =
+        d_vec[:-1].reshape(-1, A_size, A_size).trace(axis1=1, axis2=2)
 
     drho_t = d_vec[-1]
     drho_t += np.dot(dW, d_vec[:-1])
@@ -126,16 +136,15 @@ cdef _rhs_rho_euler_homodyne_fast(complex[::1] rho_t,
 @cython.wraparound(False)
 cdef _rhs_rho_milstein_homodyne_single_fast(complex[::1] rho_t,
                                 complex[::1] A_data,
-                                int[::1] A_ind, int[::1] A_ptr, 
-                                int A_size, int A_len,
-                                np.ndarray[double, ndim=2] ddW):
+                                int[::1] A_ind, int[::1] A_ptr,
+                                int A_size,
+                                np.ndarray[double, ndim=1] dW):
     """
     Fast Milstein for homodyne detection with 1 stochastic operator
     """
-    np.ndarray[double, ndim=1] dW = np.copy(ddW[:, 0])
-
-    d_vec = spmv_csr(A_data, A_ind, A_ptr, rho_t).reshape(-1, len(rho_t))
-    e = np.real(
+    cdef np.ndarray[complex, ndim=2] _rhs_rho_milstein_homodyne_two_fastd_vec =
+        spmv_csr(A_data, A_ind, A_ptr, rho_t).reshape(-1, len(rho_t))
+    cdef np.ndarray[double, ndim=1] e = np.real(
         d_vec[:-1].reshape(-1, A_size, A_size).trace(axis1=1, axis2=2))
     e[1] -= 2.0 * e[0] * e[0]
 
@@ -152,16 +161,15 @@ cdef _rhs_rho_milstein_homodyne_single_fast(complex[::1] rho_t,
 @cython.wraparound(False)
 cdef _rhs_rho_milstein_homodyne_two_fast(complex[::1] rho_t,
                                 complex[::1] A_data,
-                                int[::1] A_ind, int[::1] A_ptr, 
-                                int A_size, int A_len,
-                                np.ndarray[double, ndim=2] ddW):
+                                int[::1] A_ind, int[::1] A_ptr,
+                                int A_size,
+                                np.ndarray[double, ndim=1] dW):
     """
     fast Milstein for homodyne detection with 2 stochastic operators
     """
-    np.ndarray[double, ndim=1] dW = np.copy(ddW[:, 0])
-
-    d_vec = spmv_csr(A_data, A_ind, A_ptr, rho_t).reshape(-1, len(rho_t))
-    e = np.real(
+    cdef np.ndarray[complex, ndim=2] d_vec =
+        spmv_csr(A_data, A_ind, A_ptr, rho_t).reshape(-1, len(rho_t))
+    cdef np.ndarray[double, ndim=1] e = np.real(
         d_vec[:-1].reshape(-1, A_size, A_size).trace(axis1=1, axis2=2))
     d_vec[-2] -= np.dot(e[:2][::-1], d_vec[:2])
 
@@ -181,17 +189,17 @@ cdef _rhs_rho_milstein_homodyne_two_fast(complex[::1] rho_t,
 @cython.wraparound(False)
 cdef _rhs_rho_milstein_homodyne_fast(complex[::1] rho_t,
                                 complex[::1] A_data,
-                                int[::1] A_ind, int[::1] A_ptr, 
+                                int[::1] A_ind, int[::1] A_ptr,
                                 int A_size, int sc_len,
-                                np.ndarray[double, ndim=2] ddW):
+                                np.ndarray[double, ndim=1] dW):
     """
     fast Milstein for homodyne detection with >2 stochastic operators
     """
-    cdef np.ndarray[double, ndim=1] dW = np.copy(ddW[:, 0])
-    sc2_len = 2 * sc_len
+    cdef int sc2_len = 2 * sc_len
 
-    d_vec = spmv_csr(A_data, A_ind, A_ptr, rho_t).reshape(-1, len(rho_t))
-    e = np.real(d_vec[:-1].reshape(
+    cdef np.ndarray[complex, ndim=2] d_vec =
+        spmv_csr(A_data, A_ind, A_ptr, rho_t).reshape(-1, len(rho_t))
+    cdef np.ndarray[double, ndim=1] e = np.real(d_vec[:-1].reshape(
         -1, A_size, A_size).trace(axis1=1, axis2=2))
     d_vec[sc2_len:-1] -= np.array(
         [e[m] * d_vec[n] + e[n] * d_vec[m]
