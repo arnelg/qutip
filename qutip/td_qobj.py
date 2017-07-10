743755b917edeeddf75120fa9b6cddcbf6892f27
Eric <eric.giguere@calculquebec.ca>
2017-07-10 10:49:14 -0400

    Comment td_Qobj

--- qutip/td_qobj.py
+++ qutip/td_qobj.py
@@ -1,3 +1,38 @@
+# This file is part of QuTiP: Quantum Toolbox in Python.
+#
+#    Copyright (c) 2011 and later, Paul D. Nation and Robert J. Johansson.
+#    All rights reserved.
+#
+#    Redistribution and use in source and binary forms, with or without
+#    modification, are permitted provided that the following conditions are
+#    met:
+#
+#    1. Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#    2. Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#    3. Neither the name of the QuTiP: Quantum Toolbox in Python nor the names
+#       of its contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+#    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+###############################################################################
+"""Time-dependent Quantum Object (Qobj) class.
+"""
+
 from qutip import Qobj
 from qutip.interpolate import Cubic_Spline
 from functools import partial
@@ -6,35 +41,154 @@ import numpy as np
 from numbers import Number
 from qutip.superoperator import liouvillian
 
+
 class td_Qobj:
+    """A class for representing time-dependent quantum objects,
+    such as quantum operators and states.
+
+    The td_Qobj class is a representation of time-dependent Qutip quantum
+    objects (Qobj). This class implements math operations :
+        +,- : td_Qobj, Qobj
+        * : Qobj, C-number
+        / : C-number
+    and some common linear operator/state operations. The td_Qobj
+    are constructed from a nested list of Qobj with their time-dependent
+    coefficients.
+
+    *Examples*
+
+        H = [H0, [H1, 'sin(w*t)'], [H2, f1_t], [H3, np.sin(3*w*tlist)]]
+
+        where f1_t ia a python functions with signature f_t(t, args, **kw_args).
+
+    Parameters
+    ----------
+    Q_object : array_like
+        Data for vector/matrix representation of the quantum object.
+    args : dictionary that contain the arguments for
+    tlist : array_like
+        List of times at which the numpy-array coefficients are applied. Times
+        must be equidistant and start from 0.
+    #copy : bool
+    #    Flag specifying whether Qobj should get a copy of the
+    #    input data, or use the original.
+    #check : bool
+    #    Check if the supplied Qobj are compatible
 
 
-    def __init__(self, operator=[], args={}, tlist=None):
+    Attributes
+    ----------
+    cte : Qobj
+        Constant part of the td_Qobj
+    args : map
+        arguments of the coefficients
+    const : bool
+        Indicates if quantum object is Constant
+    tlist : array_like
+        List of times at which the numpy-array coefficients are applied.
+    ops : list
+        List of Qobj and the coefficients.
+        [(Qobj, coefficient as a function, original coefficient, type), ... ]
+        type :
+            1: function
+            2: string
+            3: np.array
+    op_call : function
+        User defined function as a td_Qobj. For wrapping function Hamiltonian
+        given to solvers.
+
+
+    To implements: callback to the self.cte
+        dims : list
+            List of dimensions keeping track of the tensor structure.
+        shape : list
+            Shape of the underlying `data` array.
+        type : str
+            Type of quantum object: 'bra', 'ket', 'oper', 'operator-ket',
+            'operator-bra', or 'super'.
+        superrep : str
+            Representation used if `type` is 'super'. One of 'super'
+            (Liouville form) or 'choi' (Choi matrix with tr = dimension).
+        isherm : bool
+            Indicates if quantum object represents Hermitian operator.
+        iscp : bool
+            Indicates if the quantum object represents a map, and if that map is
+            completely positive (CP).
+        ishp : bool
+            Indicates if the quantum object represents a map, and if that map is
+            hermicity preserving (HP).
+        istp : bool
+            Indicates if the quantum object represents a map, and if that map is
+            trace preserving (TP).
+        iscptp : bool
+            Indicates if the quantum object represents a map that is completely
+            positive and trace preserving (CPTP).
+            Indicates if the quantum object represents a map that is completely
+            positive and trace preserving (CPTP).
+        isket : bool
+            Indicates if the quantum object represents a ket.
+        isbra : bool
+            Indicates if the quantum object represents a bra.
+        isoper : bool
+            Indicates if the quantum object represents an operator.
+        issuper : boolt
+            Indicates if the quantum object represents a superoperator.
+        isoperket : bool
+            Indicates if the quantum object represents an operator in column vector
+            form.
+        isoperbra : bool
+            Indicates if the quantum object represents an operator in row vector
+            form.
+
+    Methods
+    -------
+    apply(f, *args, **kw_args)
+        Apply the function f to every Qobj. f(Qobj) -> Qobj
+        Return a modified td_Qobj and let the original one untouched
+    copy()
+        Create copy of Qobj
+    conj()
+        Conjugate of quantum object.
+    dag()
+        Adjoint (dagger) of quantum object.
+    permute(order)
+        Returns composite qobj with indices reordered.
+    ptrace(sel)
+        Returns quantum object for selected dimensions after performing
+        partial trace.
+    tidyup(atol=1e-12)
+        Removes small elements from quantum object.
+    trans()
+        Transpose of quantum object.
+    """
+
+    def __init__(self, Q_object=[], args={}, tlist=None):
         self.const = False
         self.args = args
         self.cte = None
         self.tlist  = tlist
         self.op_call = None
 
-        op_type = self._td_format_check_single(operator, tlist)
+        op_type = self._td_format_check_single(Q_object, tlist)
         self.op_type = op_type
         self.ops = []
 
         if isinstance(op_type, int):
             if op_type == 0:
-                self.cte = operator
+                self.cte = Q_object
                 self.const = True
-                if operator.issuper :
+                if Q_object.issuper :
                     self.issuper = True
-            elif op_type == 1: #a function, no test to see if the function does return a Qobj.
-                self.op_call = operator
-            elif op_type == -1: #a function, no test to see if the function does return a Qobj.
+            elif op_type == 1:
+                #a function, no test to see if the function does return a Qobj.
+                self.op_call = Q_object
+            elif op_type == -1:
                 pass
 
         else:
             compile_list = []
             compile_count = 0
-            for type_, op in zip(op_type, operator):
+            for type_, op in zip(op_type, Q_object):
                 if type_ == 0:
                     if self.cte == None:
                         self.cte = op
@@ -51,7 +205,7 @@ class td_Qobj:
                     self.ops.append([op[0], None,
                             op[1].copy(), 3])
 
-                    self.ops[-1][1] = lambda t,*args,l=l,**kw_args: (0 if (t > self.tlist[-1]) 
+                    self.ops[-1][1] = lambda t,*args,l=l,**kw_args: (0 if (t > self.tlist[-1])
                             else self.ops[l][2][int(round((len(self.tlist)-1) * (t/self.tlist[-1])))])
                 else:
                     raise Exception("Should never be here")
@@ -63,7 +217,7 @@ class td_Qobj:
                     if op[3] == 2:
                         op[1] = str_funcs[count]
                         count += 1
-            
+
             if not self.cte:
                 self.cte = self.ops[0][0]
                 for op in self.ops[1:]:
@@ -86,8 +240,8 @@ class td_Qobj:
 
     def _td_array_to_str(self, op_np2, times):
         """
-        Wrap numpy-array based time-dependence in the string-based time dependence
-        format
+        Wrap numpy-array based time-dependence in the string-based
+        time dependence format
         """
         n = 0
         str_op = []
@@ -103,22 +257,22 @@ class td_Qobj:
 
         return str_op,np_args
 
-    def _td_format_check_single(self, operator, tlist=None):
+    def _td_format_check_single(self, Q_object, tlist=None):
         op_type = []
 
-        if isinstance(operator, Qobj):
+        if isinstance(Q_object, Qobj):
             op_type = 0
-        elif isinstance(operator, (FunctionType, BuiltinFunctionType, partial)):
-            op_type = 1 
-        elif isinstance(operator, list):
-            if (len(operator) == 0 ):
+        elif isinstance(Q_object, (FunctionType, BuiltinFunctionType, partial)):
+            op_type = 1
+        elif isinstance(Q_object, list):
+            if (len(Q_object) == 0 ):
                 op_type = -1
-            for op_k in operator:
+            for op_k in Q_object:
                 if isinstance(op_k, Qobj):
                     op_type.append(0)
                 elif isinstance(op_k, list):
                     if not isinstance(op_k[0], Qobj):
-                        raise TypeError("Incorrect operator specification")
+                        raise TypeError("Incorrect Q_object specification")
                     elif len(op_k) == 2:
                         if isinstance(op_k[1], (FunctionType,
                                                BuiltinFunctionType, partial)):
@@ -133,14 +287,14 @@ class td_Qobj:
                             raise NotImplementedError("Cubic_Spline not supported")
                         #    h_obj.append(k)
                         else:
-                            raise TypeError("Incorrect operator specification")
+                            raise TypeError("Incorrect Q_object specification")
                     else:
-                        raise TypeError("Incorrect operator specification")
+                        raise TypeError("Incorrect Q_object specification")
 
-
+str_func
 
         else:
-            raise TypeError("Incorrect operator specification")
+            raise TypeError("Incorrect Q_object specification")
 
         return op_type
 
@@ -156,7 +310,7 @@ class td_Qobj:
                 return compiled_str_coeff(t,*(list(zip(*self.args))[1]))
 
         return compiled_str_coeff
-        
+
 
     def _compile_str_single(self, compile_list):
 
@@ -263,7 +417,7 @@ def """ + func_name +"(double t"
                 new.ops[-1][1] = op[1]
             elif new.ops[-1][3] == 3:
                 l = len(new.ops)-1
-                new.ops[l][1] = lambda t,*args,l=l,**kw_args: (0 if (t > self.tlist[-1]) 
+                new.ops[l][1] = lambda t,*args,l=l,**kw_args: (0 if (t > self.tlist[-1])
                         else self.ops[l][2][int(round((len(self.tlist)-1) * (t/self.tlist[-1])))])
         return new
 
@@ -349,19 +503,6 @@ def """ + func_name +"(double t"
     def __truediv__(self, other):
         return self.__div__(other)
 
-    def __pow__(self, n, m=None):
-        res = self.copy()
-        res.cte = self.cte.__pow__(n,m)
-        for op in res.ops:
-            op[0] = op[0].__pow__(n,m)
-        return res
-
-    def __ipow__(self, n, m=None):
-        self.cte = self.cte.__pow__(n,m)
-        for op in self.ops:
-            op[0] = op[0].__pow__(n,m)
-        return self
-
     def __neg__(self):
         res = self.copy()
         res.cte = (-res.cte)
@@ -381,14 +522,14 @@ def """ + func_name +"(double t"
         res.cte = res.cte.conj()
         for op in res.ops:
             op[0] = op[0].conj()
-        return res       
+        return res
 
     def dag(self):
         res = self.copy()
         res.cte = res.cte.dag()
         for op in res.ops:
             op[0] = op[0].dag()
-        return res      
+        return res
 
     def apply(self, function, *args, **kw_args):
         res = self.copy()
@@ -398,13 +539,31 @@ def """ + func_name +"(double t"
         res.cte = cte_res
         for op in res.ops:
             op[0] = function(op[0], *args, **kw_args)
-        return res 
+        return res
 
+    def tidyup(self,atol=1e-12):
+        self.cte = self.cte.tidyup(atol)
+        for op in res.ops:
+            op[0] = op[0].tidyup(atol)
+        return res
 
+    def permute(self,order):
+        res = self.copy()
+        res.cte = res.cte.permute(order)
+        for op in res.ops:
+            op[0] = op[0].permute(order)
+        return res
+
+    def ptrace(self,sel):
+        res = self.copy()
+        res.cte = res.cte.ptrace(sel)
+        for op in res.ops:
+            op[0] = op[0].ptrace(sel)
+        return res
 
 def td_liouvillian(H, c_ops=[],  chi=None):
     """Assembles the Liouvillian superoperator from a Hamiltonian
-    and a ``list`` of collapse operators. Accept time dependant 
+    and a ``list`` of collapse operators. Accept time dependant
     operator and return a td_qobj
 
     Parameters
